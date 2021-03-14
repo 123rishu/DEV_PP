66e5e26db03b9e3ff0fea0018d99e6ac35ae449c
Rishabh <sharmarishabh417@gmail.com>
2021-03-14 14:56:43 +0530

    highest wickettaker upgraded

--- lec4_Web_Scrapping/activity/highestWicketTaker.js
+++ lec4_Web_Scrapping/activity/highestWicketTaker.js
@@ -18,19 +18,43 @@ function parseData(html){
     let highestWicketSoFar = 0;
     let nameOfHighestWicketTaker;
     let economy;
+    let teamName;
+    let opponentTeamName;
 
     let ch = cheerio.load(html);
-    let bothBowlingTables = ch('.Collapsible .table.bowler');
+    let bothScorecards = ch('.Collapsible');
+    let bothTeamName = bothScorecards.find(".Collapsible h5");
+    let teamNameOne = ch(bothTeamName['0']).text();
+    let teamNameTwo = ch(bothTeamName['1']).text();
+    teamNameOne = teamNameOne.split("INNINGS")[0].trim();
+    teamNameTwo = teamNameTwo.split("INNINGS")[0].trim();
+    //console.log(bothTeamName);
+    //console.log(teamNameOne);
+    //console.log(teamNameTwo);
+    //{ '0': {}, '1': {} }
+    //let bothBowlingTables = ch('Collapsible .table.bowler.');
     //{ '0': {}, '1': {} }
 
-    for(let i=0;i<bothBowlingTables.length;i++){
-        let bowlingTable = bothBowlingTables[`${i}`];
+    for(let i=0;i<bothScorecards.length;i++){
+        let scorecard = ch(bothScorecards[`${i}`]);
+        let bowlingTable = scorecard.find(".table.bowler");
+        let opponentCurTeamName = scorecard.find(".Collapsible h5").text();
+        opponentCurTeamName = opponentCurTeamName.split("INNINGS")[0].trim();
+        //console.log(teamName);
         let allTrs = ch(bowlingTable).find("tbody tr");
-        // { 0:tr, 1:tr}
+        // // { 0:tr, 1:tr}
         for(let j=0;j<allTrs.length;j++){
             let allTds = ch(allTrs[j]).find("td");
             let wicketsTaken = ch(allTds['4']).text();
             if(wicketsTaken > highestWicketSoFar){
+                if(opponentCurTeamName === teamNameTwo){
+                   teamName = teamNameOne;
+                   opponentTeamName = opponentCurTeamName;
+                }
+                else if(opponentCurTeamName === teamNameOne){
+                    teamName = teamNameTwo;
+                    opponentTeamName = opponentCurTeamName;
+                }
                 highestWicketSoFar = wicketsTaken;
                 nameOfHighestWicketTaker = ch(allTds['0']).text();
                 economy = ch(allTds['5']).text();
@@ -38,7 +62,11 @@ function parseData(html){
         }
 
     }
+
+
     highestWicketTaker.name = nameOfHighestWicketTaker;
+    highestWicketTaker.teamKaName = teamName;
+    highestWicketTaker.opponentTeamKaName = opponentTeamName;
     highestWicketTaker.wickets = highestWicketSoFar;
     highestWicketTaker.economy = economy;
     console.log(highestWicketTaker);
