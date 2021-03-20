7f00803228e545e83d646c4e9195ca86c65ddb4c
Rishabh <sharmarishabh417@gmail.com>
2021-03-20 22:04:07 +0530

    test qstns

---
+++ lec8_Github_sirWay/practice.js
@@ -0,0 +1,30 @@
+// function decToBin(n) {
+//     // write code here
+    
+//     let num = 0;
+//     let pow = 1;
+    
+//     while(n>0){
+//         let rem = n%2;
+        
+//         num = num + (rem * pow);
+//         pow = pow*10;
+//         n = n/2;
+//     }
+//     return num;
+// }
+
+
+// let ans = decToBin(45);
+// console.log(ans);
+
+
+
+// let obj = {"concept":""};
+
+
+// console.log(
+//   JSON.parse(
+//     JSON.stringify(obj).slice(0, 12) + "JSON" + JSON.stringify(obj).slice(12)
+//   ).concept
+// );
