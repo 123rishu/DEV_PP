5369afa34579e88853595d638c101eaa516f570a
Rishabh <sharmarishabh417@gmail.com>
2021-04-04 22:12:42 +0530

    TEST2

---
+++ JSTests/Test2/sol/q6.js
@@ -0,0 +1,16 @@
+// function fun(){
+    
+// }
+
+// fun();
+
+//setTimeout(function(){ console.log("Hello"); }, 10000);
+
+
+//setInterval(function(){ console.log("Hello"); }, 3000);
+
+let n=0;
+while(n<10){
+    setTimeout(function(){ console.log("Hello"); n++; }, 2000);
+
+}
