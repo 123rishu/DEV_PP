2b2c3a62657f68f0b0fd4cd8f86a31c652db61c7
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-05-03 16:40:09 +0530

    cells positioning done

--- Module2_DOM/Lec3_Excel_Clone/activity/script.js
+++ Module2_DOM/Lec3_Excel_Clone/activity/script.js
@@ -1,30 +1,46 @@
-let cellContent = document.querySelector(".cells-content");
+let cellsContent = document.querySelector(".cells-content");
 
 function initCells(){
-    // <div class="row">
-    // <div class="cell"></div>
-    // <div class="cell"></div>
-    // <div class="cell"></div>
-    // <div class="cell"></div>
-    // <div class="cell"></div>
-    // <div class="cell"></div> so on .... 26 cells in one row and 100 total rows
-    // </div>
+    let cells = `<div class="top-left-cell"></div>`;
+    cells += `<div class="top-row">`;
+    for(let i=0;i<26;i++){
+        cells += `<div class="top-row-cell">${String.fromCharCode(65+i)}</div>`;
+    }
+    cells += `</div>`;
 
-    let cells = "";
+    cells += `<div class="left-col">`;
+    for(let i=0;i<100;i++){
+        cells += `<div class="left-col-cell">${i+1}</div>`;
+    }
+    cells += `</div>`;
 
+    cells += `<div class="cells">`;
     for(let i=0;i<100;i++){
         cells += `<div class="row">`
         for(let j=0;j<26;j++){
-            cells += `<div class="cell">Cell</div>`
+            cells += `<div class="cell" contentEditable="true"></div>`
         }
         cells += `</div>`
     }
-
-    cellContent.innerHTML = cells;
+    cells += `</div>`
+    cellsContent.innerHTML = cells;
 }
 initCells();
 
+let topRow = document.querySelector(".top-row");
+let leftCol = document.querySelector(".left-col");
+let topLeftCell = document.querySelector(".top-left-cell");
+console.log(topLeftCell);
 
+cellsContent.addEventListener("scroll", function(e){
+    let top = e.target.scrollTop;
+    let left = e.target.scrollLeft;
+    //console.log(e);
+    topRow.style.top = top+"px";
+    topLeftCell.style.top =top+"px";
+    topLeftCell.style.left =left+"px";
+    leftCol.style.left =left+"px";
+})
 
 
 
