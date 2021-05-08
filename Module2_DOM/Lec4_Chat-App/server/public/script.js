653ecd5e9766b5e687546dbbfba642748d4a75fe
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-05-08 18:08:46 +0530

    complete chat app

--- Module2_DOM/Lec4_Chat-App/server/public/script.js
+++ Module2_DOM/Lec4_Chat-App/server/public/script.js
@@ -1,7 +1,10 @@
 let chatInput = document.querySelector(".chat-input-box");
 let chatWindow = document.querySelector(".chat-window");
+let myName = document.querySelector(".me .user-name");
 
 let username = prompt("Enter your name");
+myName.textContent = username;
+
 
 chatInput.addEventListener("keypress", function(e){
     if(e.key == "Enter"){
@@ -10,6 +13,8 @@ chatInput.addEventListener("keypress", function(e){
         chatDiv.classList.add("right");
         chatDiv.textContent = username + " : " + chatInput.value;
         chatWindow.append(chatDiv);
+        //emit chat message and your name
+        socket.emit("chat", {username, chat: chatInput.value});
         chatInput.value = "";
     }
 })
