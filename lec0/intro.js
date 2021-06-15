99d2dc1d499f7fb94b6e954137c559266c31a83c
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-06-16 00:17:38 +0530

    sorting numbers using script

--- lec0/intro.js
+++ lec0/intro.js
@@ -10,3 +10,18 @@ let updatedNames = names.map(function(curr){
 })
 
 console.log(updatedNames);
+
+let arr = [5, 4, 1, 2, 10, 20, 6];
+
+
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]> arr[j]){
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+}
+
+console.log(arr);
