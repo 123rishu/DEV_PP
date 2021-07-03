c1111b15cdb80552084539028671ff70de6e1306
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-07-03 23:11:29 +0530

    useState Hook

--- Module3_React/hooks/src/App.js
+++ Module3_React/hooks/src/App.js
@@ -1,12 +1,9 @@
 import UseState from "./Components/UseState";
-import UseEffect from "./Components/UseEffect";
-import HookTwo from "./Components/UseEffect";
 
 function App() {
   return (
     <div className="App">
-      {/* <UseState></UseState> */}
-      <HookTwo></HookTwo>
+      <UseState></UseState>
     </div>
   );
 }
