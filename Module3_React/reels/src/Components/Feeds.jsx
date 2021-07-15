c16ba619c9b88192904b9c4480a06ec3ca3b3549
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-07-15 21:53:32 +0530

    reels added from firebase + intersectionobserver done

--- Module3_React/reels/src/Components/Feeds.jsx
+++ Module3_React/reels/src/Components/Feeds.jsx
@@ -1,21 +1,23 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { AuthContext } from '../context/AuthProvider';
-import {Button} from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
 import { firebaseDB, firebaseStorage } from "../config/firebase";
 import { v4 as uuidv4 } from 'uuid';
+import VideoPosts from './VideoPosts';
 
 const Feeds = (props) => {
-    const {currUser} = useContext(AuthContext);
+    const { currUser } = useContext(AuthContext);
     const [videoFile, setVideoFile] = useState(null);
-    const {signOut} = useContext(AuthContext);
+    const [posts, setPosts] = useState([]);
+    const { signOut } = useContext(AuthContext);
 
     const handleLogOut = async () => {
-        try{
+        try {
             await signOut();
             props.history.push("/login");
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
     }
@@ -26,31 +28,31 @@ const Feeds = (props) => {
     }
 
     const handleUploadFile = async () => {
-        try{
+        try {
             let uid = currUser.uid;
             let uploadVideoObject = firebaseStorage.ref(`/profile/${uid}/${Date.now()}.mp4`).put(videoFile);
 
-            uploadVideoObject.on("state_changed", fun1,fun2,fun3);
-            function fun1(snapshot){
-                let progress = (snapshot.bytesTransferred/ snapshot.totalBytes ) * 100;
+            uploadVideoObject.on("state_changed", fun1, fun2, fun3);
+            function fun1(snapshot) {
+                let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
                 console.log(progress);
-            }   
+            }
 
-            function fun2(error){
+            function fun2(error) {
                 console.log(error);
             }
 
-            async function fun3(){
+            async function fun3() {
                 let videoFileUrl = await uploadVideoObject.snapshot.ref.getDownloadURL();
                 console.log(videoFileUrl);
                 let pid = uuidv4();
-
-                firebaseDB.collection("posts").doc(pid).set({
-                    pid:pid,
-                    uid:uid,
-                    comments:[],
-                    likes:[],
-                    videoLink:videoFileUrl,
+                console.log(pid);
+                await firebaseDB.collection("posts").doc(pid).set({
+                    pid: pid,
+                    uid: uid,
+                    comments: [],
+                    likes: [],
+                    videoLink: videoFileUrl,
                 });
 
                 let doc = await firebaseDB.collection("users").doc(uid).get();
@@ -60,12 +62,61 @@ const Feeds = (props) => {
                 await firebaseDB.collection("users").doc(uid).set(document);
             }
         }
-        catch(err){
+        catch (err) {
             console.log(err);
         }
     }
 
-    return ( 
+    let conditionObject = {
+        root: null, //observe from whole page
+        threshold: "0.8", //80%
+    };
+
+    function cb(entries) {
+        console.log(entries);
+        entries.forEach((entry) => {
+            let child = entry.target.children[0];
+            // play(); => async
+            // pause(); => sync
+
+            child.play().then(function () {
+                if (entry.isIntersecting == false) {
+                    child.pause();
+                }
+            });
+        });
+    }
+
+    useEffect(() => {
+        // code which will run when the component loads
+        let observerObject = new IntersectionObserver(cb, conditionObject);
+        let elements = document.querySelectorAll(".video-container");
+
+        elements.forEach((el) => {
+            observerObject.observe(el); //Intersection Observer starts observing each video element
+        });
+    }, [posts]);
+
+    useEffect(() => {
+        //GET ALL THE POSTS
+        // get all documents of a collection in firebase
+        //   db.collection('documents')
+        // .get()
+        // .then(querySnapshot => {
+        //   const documents = querySnapshot.docs.map(doc => doc.data())
+        //   // do something with documents
+        firebaseDB
+            .collection("posts")
+            .get()
+            .then((snapshot) => {
+                let allPosts = snapshot.docs.map((doc) => {
+                    return doc.data();
+                });
+                setPosts(allPosts);
+            });
+    }, []);
+
+    return (
         <div>
             <h1>Feeds</h1>
             <button onClick={handleLogOut}>Logout</button>
@@ -82,11 +133,20 @@ const Feeds = (props) => {
                         >
                             Upload Video
                         </Button>
-                        </label>
+                    </label>
                 </div>
             </div>
+
+            <div className="feeds-video-list">
+                {
+                    posts.map((currPostObj) => {
+                        return <VideoPosts key={currPostObj.pid} postObj={currPostObj}></VideoPosts>
+                    })
+                }
+            </div>
+
         </div>
-     );
+    );
 }
- 
+
 export default Feeds;
