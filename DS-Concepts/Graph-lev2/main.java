f5d13ce962fdac0b727bc55b132754fd76c322ce
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-08-10 23:30:17 +0530

    optimized DSU done

--- DS-Concepts/Graph-lev2/main.java
+++ DS-Concepts/Graph-lev2/main.java
@@ -1,12 +1,17 @@
+import java.util.*;
+
 public class main{
 
     static int[] parent;
+    static int[] rank;
 
     public static int find(int x){
         if(parent[x] == x){
             return x;
         }
         int temp = find(parent[x]);
+        //path compression
+        parent[x] = temp;
         return temp;
     }
 
@@ -17,14 +22,25 @@ public class main{
 
         //x will point to y
         if(lx != ly){
-            //union ---> 
-            parent[x] = y;
+            //union on the basis of rank
+            if(rank[lx] < rank[ly]){
+                //greater rank wala parent banega, different ranks ke case me
+                parent[lx] = ly;
+            }
+            else if(rank[lx] > rank[ly]){
+                parent[ly] = lx;
+            }
+            else{
+                //when ranks of the elements are equal, koi bhi parent ban sakta hai
+                parent[lx] = ly;
+                rank[ly]++;
+            }
         }
     }
 
     public static void main(String[] args){
         //create a 2d relation array
-        int[][] relations = {{1,3}, {3,2}, {5,9}, {6,3}, {10,5}, {7,5}, {8,2}};
+        int[][] relations = {{1,3}, {3,2}, {5,9}, {6,3}, {10,5}, {7,5}, {8,2}, {5,2}};
 
         //create a parent array static and then initialize it
         parent = new int[11];
@@ -33,6 +49,7 @@ public class main{
         for(int i=1;i<11;i++){
             parent[i] = i;
         }
+        rank = new int[parent.length];
 
         //now takes values from each relation, and then perform their union one by one
         for(int i=0;i<relations.length;i++){
@@ -49,5 +66,32 @@ public class main{
             }
         }
         System.out.println(groups);
+
+        //now, hashmap ki help se saare group ko display karo, along with their leader name as key values
+        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
+
+        //first apply find using path compression on all elements of parent array
+        for(int i=1;i<parent.length;i++){
+            find(i);
+        }
+
+        for(int i=1;i<parent.length;i++){
+            int grp = parent[i];
+            int element = i;
+            if(map.containsKey(grp) == true){
+                //already hai grp leader toh, list fetch karo usme element add maro then list update maro
+                ArrayList<Integer> list = map.get(grp);
+                list.add(element);
+                map.put(grp, list);
+            }
+            else{
+                //agar grp leader nahi hai, toh list create karo, element dalo aur list push maro hashmap ke andar
+                ArrayList<Integer> list = new ArrayList<>();
+                list.add(element);
+                map.put(grp, list);
+            }
+        }
+
+        System.out.println(map);
     }
 }
