08ca64b92e7bda6671ab20b465f22e5fe91a9d92
Rishabh Sharma <sharmarishabh417@gmail.com>
2021-08-23 21:43:16 +0530

    code cleaned

--- Module3_React/movies/src/Components/Movies/Movies.jsx
+++ Module3_React/movies/src/Components/Movies/Movies.jsx
@@ -2,24 +2,20 @@ import React, { Component } from 'react';
 import Movie from "../Movie/Movie";
 import "./Movies.css";
 
-class Movies extends Component {
-  state = {  }
-  render() { 
-    let moviesData = this.props.movies;
-    let myLikedMovies = this.props.myLikedMovies;
-    return ( 
-      <div className="movies">
-        {
-          moviesData.map(function(currMovieObj){
-            return <Movie 
-            key={currMovieObj.id} 
+const Movies = (props) => {
+  let moviesData = props.movies;
+  return (
+    <div className="movies">
+      {
+        moviesData.map(function (currMovieObj) {
+          return <Movie
+            key={currMovieObj.id}
             movieObject={currMovieObj}
-            ></Movie>
-          })
-        }
-      </div>
-     );
-  }
+          ></Movie>
+        })
+      }
+    </div>
+  );
 }
- 
+
 export default Movies;
